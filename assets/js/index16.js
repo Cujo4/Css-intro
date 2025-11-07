cee4691b44516648f90fac4375b6343b58631b98
Cujo <temp.sergey4@gmail.com>
2025-11-07 21:48:45 +0200

    StartJS

---
+++ assets/js/index16.js
@@ -0,0 +1,19 @@
+// alert("Hello world!");
+
+// console.log("Hi)))");
+
+// color: white; - однострочный комментарий;
+
+// /* color: white; */ - многострочный комментарий;
+
+let userName = "Test"; // let - Можно менять значение;
+
+console.log("userName :>>", userName);
+
+userName = "235, 5, 17";
+
+console.log(userName);
+
+const constValue = 10; // const - Нельзя менять значение;
+
+console.log(constValue);
